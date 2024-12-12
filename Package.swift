770a86b17c941557469d95a3222777923569af8b
Te993 <3923106166@qq.com>
2024-12-12 17:21:32 +0800

    add llava for swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,31 @@ let package = Package(
     ],
     products: [
         .library(name: "llama", targets: ["llama"]),
+        .library(name: "llava", targets: ["llava"]),  // 添加 llava 库
     ],
     targets: [
         .systemLibrary(name: "llama", pkgConfig: "llama"),
+        .target(
+            name: "llava",
+            dependencies: ["llama"],  // 依赖 llama
+            path: "examples/llava",   // 指定 llava 源码路径
+            exclude: [
+                "android",           // 排除不需要的文件和目录
+                "llava-cli.cpp"
+            ],
+            sources: [              // 指定源文件
+                "llava.cpp",
+                "clip.cpp"
+            ],
+            cSettings: [            // C 编译设置
+                .headerSearchPath("."),
+                .headerSearchPath("../."),
+                .headerSearchPath("../../common"),
+                .headerSearchPath("../../")
+            ],
+            cxxSettings: [          // C++ 编译设置
+                .unsafeFlags(["-std=c++11"])
+            ]
+        )
     ]
 )
